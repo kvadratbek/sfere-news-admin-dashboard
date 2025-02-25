050b162abaf020ec316b55822d8075ac6a16d89c
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-02-25 13:29:16 +0500

    feed items interface fox

--- src/shared/model/feed-items/feed-item-types.ts
+++ src/shared/model/feed-items/feed-item-types.ts
@@ -1,22 +1,22 @@
 interface IFeedItem {
   category_id: number;
-  content: string;
-  created_at: string;
+  content?: string;
+  created_at?: string;
   description: string;
   feed_id: number;
-  id: number;
+  id?: number;
   lang: string;
   link: string;
-  pulished_at: string;
+  pulished_at?: string;
   thumbnails: {
     alt_text: string;
     dld_url: string;
-    height: number;
-    src_url: string;
-    width: number;
+    height?: number;
+    src_url?: string;
+    width?: number;
   };
   title: string;
-  updated_at: string;
+  updated_at?: string;
 }
 
 export type { IFeedItem };
