586e40215fc34896965d5fa9d69a12c5de684fe5
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-03-06 14:11:01 +0500

    router with token verification

---
+++ src/app/routers/app-router-protected.tsx
@@ -0,0 +1,66 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ProtectedRoute } from "./protected-route"; // Adjust path as needed
+import {
+  LoginPage,
+  DashboardPage,
+  FeedCategoriesPage,
+  FeedsPage,
+  FeedContentsPage,
+  FeedItemsPage,
+} from "@/pages";
+import { Callback } from "@/features/authentication";
+
+export const AppRouter = () => {
+  const routerConfig = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginPage />,
+    },
+    {
+      path: "/callback",
+      element: <Callback />,
+    },
+    {
+      path: "/dashboard",
+      element: (
+        <ProtectedRoute>
+          <DashboardPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/feed-categories",
+      element: (
+        <ProtectedRoute>
+          <FeedCategoriesPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/feeds",
+      element: (
+        <ProtectedRoute>
+          <FeedsPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/feeds/:feedId",
+      element: (
+        <ProtectedRoute>
+          <FeedContentsPage />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/feed-items",
+      element: (
+        <ProtectedRoute>
+          <FeedItemsPage />
+        </ProtectedRoute>
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={routerConfig} />;
+};
