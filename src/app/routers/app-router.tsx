3c17111ae29f30b50aa0d84e082eadd50c6fddfb
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-03-06 13:56:10 +0500

    login and callback imported

--- src/app/routers/app-router.tsx
+++ src/app/routers/app-router.tsx
@@ -1,16 +1,26 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
+  LoginPage,
   DashboardPage,
   FeedCategoriesPage,
   FeedsPage,
   FeedContentsPage,
   FeedItemsPage,
 } from "@/pages";
+import { Callback } from "@/features/authentication";
 
 export const AppRouter = () => {
   const routerConfig = createBrowserRouter([
     {
       path: "/",
+      element: <LoginPage />,
+    },
+    {
+      path: "/callback",
+      element: <Callback />,
+    },
+    {
+      path: "/dashboard",
       element: <DashboardPage />,
     },
     {
