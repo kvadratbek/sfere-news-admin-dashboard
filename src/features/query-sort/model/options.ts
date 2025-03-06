31e8f711e9fc28cbadf63cdc60d137ee5bee1a79
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-03-06 14:34:21 +0500

    query sort options created

---
+++ src/features/query-sort/model/options.ts
@@ -0,0 +1,28 @@
+const OPTIONS = [
+  {
+    value: "feed_id",
+    name: "Feed ID",
+  },
+  {
+    value: "feed_category_id",
+    name: "Catefory ID",
+  },
+  {
+    value: "feed_item_id",
+    name: "Item ID",
+  },
+  {
+    value: "lang",
+    name: "Language",
+  },
+  {
+    value: "published_at",
+    name: "Published At",
+  },
+  {
+    value: "title",
+    name: "Title",
+  },
+];
+
+export { OPTIONS };
