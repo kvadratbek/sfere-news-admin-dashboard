8f57ca14490d8f850de3fa51b39c07be3bc5438c
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-03-06 14:41:20 +0500

    header interface unnecessary types removed

--- src/widgets/app-header/model/types.ts
+++ src/widgets/app-header/model/types.ts
@@ -1,9 +1,6 @@
 interface IAppHeader {
-  isInDashboardPage: boolean;
   breadcrumbItemName?: string;
   breadcrumbFeedId?: string;
-  hasLangSelector: boolean;
-  actionButton?: JSX.Element;
 }
 
 export type { IAppHeader };
