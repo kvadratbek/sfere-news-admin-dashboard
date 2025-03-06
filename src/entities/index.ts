f4b6d5865a0128f6ee3fed8a047b97d0f620c341
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-03-06 14:11:52 +0500

    query filter export added

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,5 +2,6 @@ import { FeedCategory } from "./feed-category";
 import { Feed } from "./feed";
 import { FeedContent } from "./feed-content";
 import { FeedItem } from "./feed-item";
+import { QueryFilter } from "./query-filter";
 
-export { FeedCategory, Feed, FeedContent, FeedItem };
+export { FeedCategory, Feed, FeedContent, FeedItem, QueryFilter };
