0f2337d637ac37cb1476c87c6b6b112347419417
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-03-13 10:30:19 +0500

    types edited

--- src/shared/model/feed-items/feed-item-types.ts
+++ src/shared/model/feed-items/feed-item-types.ts
@@ -1,24 +1,24 @@
 interface IFeedItem {
   category_id: number;
-  content?: string;
-  created_at?: string;
+  content: string;
+  created_at: string;
   description: string;
   feed_id: number;
   id: number;
   lang: string;
   link: string;
-  published_at?: string;
-  soruce_url?: string;
-  source_title?: string;
-  thumbnails?: {
-    alt_text?: string;
-    dld_url?: string;
-    height?: number;
-    src_url?: string;
-    width?: number;
+  pulished_at: string;
+  soruce_url: string;
+  source_title: string;
+  thumbnails: {
+    alt_text: string;
+    dld_url: string;
+    height: number;
+    src_url: string;
+    width: number;
   };
   title: string;
-  updated_at?: string;
+  updated_at: string;
 }
 
 export type { IFeedItem };
