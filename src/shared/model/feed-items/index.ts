7e1d221b3460276d317ce1ce021d47635790bef9
theinnervoiceofcorn <kudrat.kenzhaev.official@gmail.com>
2025-03-13 10:29:51 +0500

    new types exported

--- src/shared/model/feed-items/index.ts
+++ src/shared/model/feed-items/index.ts
@@ -1,10 +1,12 @@
 import { IFeedItem } from "./feed-item-types";
+import { IMutateFeedItem } from "./mutate-feed-item-types.ts";
 import { IGetAllFeedItemsParams } from "./get-all-items-params";
 import { IAllFeedItemsResponse } from "./all-feed-items-response";
 import { IGetItemsByFeedParams } from "./get-items-by-feed-params.ts";
 
 export type {
   IFeedItem,
+  IMutateFeedItem,
   IGetAllFeedItemsParams,
   IAllFeedItemsResponse,
   IGetItemsByFeedParams,
