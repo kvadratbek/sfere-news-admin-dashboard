817ad4a7c7259bdb38b4afb59f75263b70f03710
kvadratbek <kudrat.kenzhaev.official@gmail.com>
2025-03-26 13:42:21 +0500

    feeds list refactored | destructured

--- src/widgets/feeds-list/ui/feeds-list.tsx
+++ src/widgets/feeds-list/ui/feeds-list.tsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/app/store";
-import { Skeleton } from "@/shared/ui/skeleton";
 import {
   Table,
   TableBody,
-  TableCell,
   TableFooter,
-  TableHead,
-  TableHeader,
   TableRow,
+  TableCell,
 } from "@/shared/ui/table";
-import { Feed, QueryFilter } from "@/entities";
+import { Feed } from "@/entities/feed/ui/feed";
+import { QueryFilter } from "@/entities";
 import {
   AppPagination,
   QueryLanguage,
@@ -25,73 +20,24 @@ import {
   UpdateFeed,
   DeleteFeed,
 } from "@/features/feeds";
-import { useGetAllFeedsQuery } from "@/shared/api/feeds-api";
-// import { RefreshFeedItems } from "@/features/feeds/refresh-feed-items/ui/resfresh-feed-items";
+import { useFeedsList } from "../model";
+import { FeedsListLoading } from "./feeds-list-loading";
+import { FeedsListError } from "./feeds-list-error";
+import { FeedsTableHeader } from "./table-header";
 
 export const FeedsList = () => {
-  const selectedLanguage = useSelector(
-    (state: RootState) => state.language.selectedLanguage
-  );
-  const [currentPage, setCurrentPage] = useState(1);
-  const [queryLimit, setQueryLimit] = useState(100);
-  const [queryPriority, setQueryPriority] = useState(true);
-  const { data, isLoading, error } = useGetAllFeedsQuery({
-    limit: queryLimit,
-    page: currentPage,
-    priority: queryPriority,
-    lang: selectedLanguage,
-  });
-  const totalPages = 1;
-  const dataFeeds = data;
-
-  if (isLoading) {
-    return (
-      <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="rounded-xl bg-muted/50 p-4">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>ID</TableHead>
-                <TableHead>Max Items</TableHead>
-                <TableHead>Priority</TableHead>
-                <TableHead>Lang</TableHead>
-                <TableHead>Logo</TableHead>
-                <TableHead>Title</TableHead>
-                <TableHead>Description</TableHead>
-                <TableHead>Edit</TableHead>
-                <TableHead>Delete</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell colSpan={2}>
-                    <Skeleton className="h-[20.8px] w-[full] m-[5px]" />
-                  </TableCell>
-                  <TableCell colSpan={5}>
-                    <Skeleton className="h-[20.8px] w-[full] m-[5px]" />
-                  </TableCell>
-                  <TableCell colSpan={2}>
-                    <Skeleton className="h-[20.8px] w-[full] m-[5px]" />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="text-center rounded-xl bg-muted/50 p-4">
-          ❌ Error fetching feeds. Please try again.
-        </div>
-      </div>
-    );
-  }
+  const {
+    feeds,
+    isLoading,
+    error,
+    currentPage,
+    setCurrentPage,
+    queryLimit,
+    setQueryLimit,
+    queryPriority,
+    toggleQueryPriority,
+    totalPages,
+  } = useFeedsList();
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
@@ -100,61 +46,53 @@ export const FeedsList = () => {
           <QueryLimit
             labelText="Feeds per Page"
             limitValue={queryLimit}
-            limitOnChange={(e) => setQueryLimit(e.target.value)}
+            limitOnChange={setQueryLimit}
           />
           <QueryPriority
-            onPriorityChange={() =>
-              setQueryPriority((queryPriority) => !queryPriority)
-            }
+            onPriorityChange={toggleQueryPriority}
             priorityStatus={queryPriority ? "On" : "Off"}
           />
           <QueryLanguage />
         </QueryFilter>
         <CreateFeed />
       </div>
-      <div className="w-full max-w-full overflow-x-auto rounded-xl bg-muted/50 p-4">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>ID</TableHead>
-              <TableHead>Priority</TableHead>
-              <TableHead>Lang</TableHead>
-              <TableHead>Logo</TableHead>
-              <TableHead>Title</TableHead>
-              <TableHead>Description</TableHead>
-              {/* <TableHead>Refresh</TableHead>
-              <TableHead>View Contents</TableHead>
-              <TableHead>Edit</TableHead>
-              <TableHead>Delete</TableHead> */}
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {dataFeeds?.map((feed) => (
-              <Feed
-                key={feed.id}
-                feed={feed}
-                viewContentsFeature={<ViewContents feedId={feed.id} />}
-                refreshFeature={<RefreshFeedItems id={feed.id} />}
-                updateFeature={<UpdateFeed updateFeedId={feed.id} />}
-                deleteFeature={<DeleteFeed deleteFeedId={feed.id} />}
-              />
-            ))}
-          </TableBody>
-          {totalPages > 0 && (
-            <TableFooter>
-              <TableRow>
-                <TableCell colSpan={9}>
-                  <AppPagination
-                    currentPage={currentPage}
-                    totalPages={totalPages}
-                    onPageChange={setCurrentPage}
-                  />
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          )}
-        </Table>
-      </div>
+
+      {isLoading ? (
+        <FeedsListLoading />
+      ) : error ? (
+        <FeedsListError />
+      ) : (
+        <div className="w-full max-w-full overflow-x-auto rounded-xl bg-muted/50 p-4">
+          <Table>
+            <FeedsTableHeader />
+            <TableBody>
+              {feeds?.map((feed) => (
+                <Feed
+                  key={feed.id}
+                  feed={feed}
+                  viewContentsFeature={<ViewContents feedId={feed.id} />}
+                  refreshFeature={<RefreshFeedItems id={feed.id} />}
+                  updateFeature={<UpdateFeed updateFeedId={feed.id} />}
+                  deleteFeature={<DeleteFeed deleteFeedId={feed.id} />}
+                />
+              ))}
+            </TableBody>
+            {totalPages > 0 && (
+              <TableFooter>
+                <TableRow>
+                  <TableCell colSpan={10}>
+                    <AppPagination
+                      currentPage={currentPage}
+                      totalPages={totalPages}
+                      onPageChange={setCurrentPage}
+                    />
+                  </TableCell>
+                </TableRow>
+              </TableFooter>
+            )}
+          </Table>
+        </div>
+      )}
     </div>
   );
 };
